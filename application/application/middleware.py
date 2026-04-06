292bb44157b7a496474740ce42ac01fb42f70d86
Ashley Osborne <ashley.osborne@prs-im.co.uk>
2026-04-06 12:44:42 +0400

    Make canonical host redirect opt-in

--- application/application/middleware.py
+++ application/application/middleware.py
@@ -8,8 +8,9 @@ class CanonicalHostMiddleware:
 
     def __call__(self, request):
         canonical_host = getattr(settings, 'CANONICAL_HOST', '')
+        enforce_canonical_host = getattr(settings, 'ENFORCE_CANONICAL_HOST', False)
 
-        if canonical_host:
+        if canonical_host and enforce_canonical_host:
             request_host = request.get_host().split(':', 1)[0]
             if request_host and request_host != canonical_host:
                 redirect_url = f"{request.scheme}://{canonical_host}{request.get_full_path()}"
